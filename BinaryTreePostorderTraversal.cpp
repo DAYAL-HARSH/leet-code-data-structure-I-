2b5e2e99b5efbd47ecc084b453a12c6c8e39c450
HARSH DAYAL <95514087+DAYAL-HARSH@users.noreply.github.com>
2022-07-20 23:37:26 +0530

    Create BinaryTreePostorderTraversal.cpp

---
+++ BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,14 @@
+recursive approach
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root != NULL){
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            ans.push_back(root->val);
+        }
+        return ans;
+    }
+};
